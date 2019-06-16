b0768f156496677c029406b44a5940588e366c21
younglust <younglust@gmail.com>
2019-06-16 18:47:57 +0200

    Create docs folder

---
+++ docs/precache-manifest.53490dbeb4808ce730d3274a5ba1de68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd61b18aef810aa7a14c1d4bc712b99",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73c3d8de1b21e0c65895",
+    "url": "./static/css/main.c4da25e1.chunk.css"
+  },
+  {
+    "revision": "a00b5167032b4cf9e61d",
+    "url": "./static/js/2.aa2dfdb1.chunk.js"
+  },
+  {
+    "revision": "73c3d8de1b21e0c65895",
+    "url": "./static/js/main.4ee53692.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
